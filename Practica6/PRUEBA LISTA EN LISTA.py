c77adaec69b0fa26d83b2746fa1e4d16d743a1a4
Francisco Llamas Ramón <56310522+fllamasramon@users.noreply.github.com>
2019-11-05 17:33:19 +0100

    Delete PRUEBA LISTA EN LISTA.py

--- Practica6/PRUEBA LISTA EN LISTA.py
+++
@@ -1,8 +0,0 @@
-palabras=["uno","dos","tres"]
-diccionario=[]
-palabra2=["cuatro","cinco","seis"]
-diccionario.append(palabras)
-diccionario.append(palabra2)
-for i in range(len(diccionario)):
-    for j in range(len(diccionario[i])):
-        print(diccionario[i][j])
